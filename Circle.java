d53cd7bf3ffe6bb7669f9862d6eeed6ab2395abe
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-03-16 22:08:20 +0700

    chu vi dien tich hinh tron

---
+++ Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    public static void main(String[] args) {
+        double radius, perimeter, acreage;
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        System.out.println("Enter radius:");
+        radius = sc.nextFloat();
+        perimeter = radius * 2 * (Math.PI);
+        acreage = radius * radius * (Math.PI);
+        System.out.println("Perimeter is:" + perimeter);
+        System.out.println("Acreage is:" + acreage);
+    }
+}
